3b4a3fdcabf1c0c574b1393956dd3f764f454d22
cmrohityadav <cmrohityadav23@gmail.com>
2023-09-29 23:22:38 +0530

    basic template

---
+++ basic/App.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>EconoVR g</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
