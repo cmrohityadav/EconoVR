81a8cb63fc6619157306066ec03a2167c8b9fa28
cmrohityadav <cmrohityadav23@gmail.com>
2023-09-30 01:47:37 +0530

    basic home page with navigation

--- basic/App.tsx
+++ basic/App.tsx
@@ -1,14 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 
-const App = () => {
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+//Navigation
+import {NavigationContainer} from "@react-navigation/native"
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+
+
+//screens
+import Home from './components/screens/Home';
+import Details from './components/screens/Details';
+
+
+
+export type RootStackParamList = {
+  Home: undefined;
+  Details: {productId: string}
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
+
+function App(): JSX.Element {
+  
+
   return (
-    <View>
-      <Text>EconoVR g</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen
+        name='Home'
+        component={Home}
+        options={{
+          title: "EconoVR"
+        }}
+        />
+        <Stack.Screen
+        name='Details'
+        component={Details}
+        options={{
+          title: "Product Details"
+        }}
+        />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-export default App
 
-const styles = StyleSheet.create({})
+
+export default App;
