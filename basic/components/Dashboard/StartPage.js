63fdb7cf800556166d5ff79cd76083f77faff8f0
cmrohityadav <cmrohityadav23@gmail.com>
2023-11-16 15:11:34 +0530

    start page of  dashboard

--- basic/components/Dashboard/StartPage.js
+++ basic/components/Dashboard/StartPage.js
@@ -5,23 +5,47 @@ const StartPage = ({ navigation }) => {
     return (
         <View style={{ backgroundColor: '#081122' }}>
 
-            <View style={{ height: 600, backgroundColor: '#FFFFFF', borderBottomEndRadius: 150, borderBottomStartRadius: 150 }}>
-                <View style={{marginTop:50 ,display:'flex',alignItems:'center',}}>
-                
-                    <Image style={{ width: 200, height:200  }}
-                        source={require('./img/ManVRGlasses.png')}
-                        
+            {/* upper container */}
+            <View style={{ height: 600, backgroundColor: '#FFFFFF', borderBottomEndRadius: 100, borderBottomStartRadius: 100, display: 'flex', alignItems: 'center' }}>
+
+                {/* head set image */}
+                <View style={{ marginTop: 50, display: 'flex', alignItems: 'center', }}>
+
+                    <Image style={{ width: 300, height: 230 }}
+                        source={require('./img/headVR.png')}
+
                     />
-                    <Image style={{ width: 300, height:132,  }}
-                        source={require('./img/Ellipse.png')}
-                    />
-                    
+
+
                 </View>
+
+                {/* heading */}
+
+                <View style={{ marginTop: 18 }}>
+                    <Text style={{ fontWeight: 'bold', fontSize: 20, color: '#000000' }}>
+                        Random VR World
+                    </Text>
+                </View>
+
+
+                {/* discription */}
+                <View style={{ marginTop: 22, marginHorizontal: 20 }}>
+                    <Text style={{ fontSize: 15, color: '#000000' }}>
+                        Description goes here Description goes here Description goes here Description goes here Description goes here Description goes here Description goes here Description goes here Description goes here
+                    </Text>
+                </View>
+
 
             </View>
 
-            <View style={{ backgroundColor: '#081122', height: 200 }}>
+            {/* lower container */}
+            <View style={{ backgroundColor: '#081122', height: 200 , display:'flex',alignItems:'center'}}>
 
+                <View style={{ marginTop:80,backgroundColor:'#FFFFFF',height:50 ,width:250,borderRadius:20,display:'flex',justifyContent:'center'}}>
+                    <Text style={{ textAlign: 'center', color: '#081122', fontSize: 30 ,fontWeight:'500'}}
+                        onPress={() => navigation.navigate('Dashboard')}
+                    >Start</Text>
+                </View>
 
             </View>
 
